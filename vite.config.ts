4163f5870c20fc2fa63f1ac059d5d518af914a9b
Sayoko123f <63059711+Sayoko123f@users.noreply.github.com>
2022-04-18 17:13:04 +0800

    build gh-page

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+    plugins: [vue()],
+    base: 'https://github.com/Sayoko123f/password-get/',
+    build: {
+        outDir: 'docs',
+    },
+});
